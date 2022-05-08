8ae6dbf57f71129d4f3f7173a9ab9610333c8a56
Eveline-97 <57260992+Eveline-97@users.noreply.github.com>
2022-05-08 20:47:46 +0200

    automatic next line and try change interval

--- assets/main.js
+++ assets/main.js
@@ -24,14 +24,50 @@ if (action != null) {
 /*SHOW AND HIDE LINES*/
 const lines = document.getElementsByClassName('text-line');
 let count = 0;
+let interval = 1000;
 
-//Event listeners for left and right arrow
+//Show and hide lines automatically
+let nextLine = () => {
+    //Show next line
+    if (count < lines.length) {
+        let line = lines[count];
+        //Get last element
+        let lastEl;
+        if (count == 0) {
+            lastEl = lines[0]
+        } else {
+            lastEl = lines[count-1];
+        }
+
+        //Only automatic if last line is not an action
+        if (lastEl.id == 'action') {
+            console.log('Waiting for arrow interaction from performers');
+        } else {
+            line.style.display = 'inline';
+            count++;
+        }
+        changeInterval();                
+    }
+    window.scrollTo(0, document.body.scrollHeight);
+}
+
+//Change interval length depending on last line's length
+let changeInterval = () => {
+    let line = lines[count];
+    let strLength = line.innerHTML.length;
+    let newInterval = strLength * 100;
+    clearInterval(interval);
+    interval = setInterval(nextLine(), newInterval);
+}
+
+changeInterval();
+
+//Event listeners for left and right arrow (only used for actions, but can also be used in between, if necessary)
 document.addEventListener('keydown', (e) => {
     if (e.code == 'ArrowRight') {
         //Show next line
         if (count < lines.length) {
             let line = lines[count];
-            line.style.display = 'inline';
             //Check last element
             let lastEl;
             if (count == 0) {
@@ -39,10 +75,13 @@ document.addEventListener('keydown', (e) => {
             } else {
                 lastEl = lines[count-1];
             }
+
+            line.style.display = 'inline';
             //If last element was an action, remove it from the document
             if (lastEl.id == 'action') {
                 lastEl.remove();
             }
+            //Recheck the document for another action object
             action = document.getElementById('action');
             count++;
         }
